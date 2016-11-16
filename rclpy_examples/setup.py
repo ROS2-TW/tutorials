7c822d0c4fc60880b322d7b4bd4ff482e829ab76
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-11-16 14:00:58 -0800

    generate executables for all rmw_implementation

--- rclpy_examples/setup.py
+++ rclpy_examples/setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup
 
+from rclpy.impl import rmw_implementation_tools
+
+rmw_impls = rmw_implementation_tools.get_rmw_implementations()
+
+entry_points = [
+    'listener_py = listener_py:main',
+    'talker_py = talker_py:main'
+    'listener_qos_py = listener_qos_py:main',
+    'talker_qos_py = talker_qos_py:main',
+]
+
+for rmw_impl in rmw_impls:
+    entry_points.append('talker_py__' + rmw_impl + ' = talker_py:main_for_rmw_impl.' + rmw_impl)
+    entry_points.append(
+        'listener_py__' + rmw_impl + ' = listener_py:main_for_rmw_impl.' + rmw_impl)
+
 setup(
     name='rclpy_examples',
     version='0.0.0',
@@ -20,12 +36,6 @@ setup(
     description='Package containing examples of how to use the rclpy API.',
     license='Apache License, Version 2.0',
     test_suite='test',
-    entry_points={
-        'console_scripts': [
-            'listener_py = listener_py:main',
-            'talker_py = talker_py:main',
-            'listener_qos_py = listener_qos_py:main',
-            'talker_qos_py = talker_qos_py:main',
-        ],
+        'console_scripts': entry_points,
     },
 )
